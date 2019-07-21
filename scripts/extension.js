72fe6d9864521fb25b290120595a38a4fda31b0d
Enes Aydın <enes.aydin@sahibinden.com>
2019-07-21 18:32:26 +0300

    getCheckedCategory

--- scripts/extension.js
+++ scripts/extension.js
@@ -23,31 +23,15 @@ button.addEventListener('click', function () {
 
 function getCheckedCategory() {
 
-    const realEstate = document.getElementById("realestate");
-    const pocketWatch = document.getElementById("pocket-watch");
-    const pocketWatchGET = document.getElementById("pocket-watch-get");
-    const teachingStuff = document.getElementById("teaching-stuff");
-    const mobilePhone = document.getElementById("mobile-phone");
-    const mobilePhoneGET = document.getElementById("mobile-phone-get");
+    const radioButtons = document.querySelectorAll(".container [name='category']");
 
-    const isCheckedRealEstate = realEstate.checked;
-    const isCheckedPocket = pocketWatch.checked;
-    const isCheckedPocketWatch = pocketWatchGET.checked;
-    const isTeachingStuff = teachingStuff.checked;
-    const isMobilePhone = mobilePhone.checked;
-    const isMobilePhoneGET = mobilePhoneGET.checked;
+    let checkedCategory = null;
 
-    if (isCheckedRealEstate) {
-        return realEstate.value;
-    } else if (isCheckedPocket) {
-        return pocketWatch.value;
-    } else if (isCheckedPocketWatch) {
-        return pocketWatchGET.value;
-    } else if (isTeachingStuff) {
-        return teachingStuff.value;
-    } else if (isMobilePhone) {
-        return mobilePhone.value;
-    } else if (isMobilePhoneGET) {
-        return mobilePhoneGET.value;
-    }
+    radioButtons.forEach(checkbox => {
+        if (checkbox.checked === true) {
+            checkedCategory = checkbox.value;
+        }
+    });
+
+    return checkedCategory;
 }
