0df0aa3fa6c1dc7aaa7a5e16fae1b1d48f49a796
Enes Aydın <enes.aydin@sahibinden.com>
2019-07-21 14:00:51 +0300

    clean code

--- scripts/content.js
+++ scripts/content.js
@@ -1,11 +1,3 @@
-let Category = {
-
-    REAL_ESTATE: "realestate",
-    POCKET_WATCH: "pocket-watch",
-    POCKET_WATCH_GET: "pocket-watch-get",
-    TEACHING_STAFF: "teaching-stuff"
-};
-
 chrome.runtime.onMessage.addListener(async function (request, sender, sendResponse) {
 
         const data = request.data;
---
+++ scripts/enums.js
@@ -0,0 +1,7 @@
+let Category = {
+
+    REAL_ESTATE: "realestate",
+    POCKET_WATCH: "pocket-watch",
+    POCKET_WATCH_GET: "pocket-watch-get",
+    TEACHING_STAFF: "teaching-stuff"
+};
